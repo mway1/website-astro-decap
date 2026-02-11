308d9da24ed69bff1c1cd4a58ae405d2c5caec72
Maxim Kuzmenko <mway2914@yandex.ru>
2026-02-11 17:21:28 +0300

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'static',
-  site: 'https://mway1.github.io/website-astro-decap',
+  site: 'https://mway1.github.io',
   base: '/website-astro-decap/',
 });
